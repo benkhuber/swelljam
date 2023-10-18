90e0b46411371a97a05a06b2151a9795787a87f1
Ben Huber <benkhuber@gmail.com>
2023-10-17 19:22:55 -0700

    Adding react router dom

---
+++ src/RegionalForecast.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BuoyData from './BuoyData';
+
+function RegionalForecast() {
+  return (
+    <div>RegionalForecast
+      <BuoyData currentStationId="46253" />
+    </div>
+  );
+}
+
+export default RegionalForecast;
