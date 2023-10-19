720429b0562d71747c854ab2a482da7aaba9ab65
Ben Huber <benkhuber@gmail.com>
2023-10-18 17:12:55 -0700

    Added spot markets to leaflet map

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-restricted-syntax */
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SpotForecast from './pages/SpotForecast';
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import {
   MapContainer, TileLayer, Marker, Popup,
 } from 'react-leaflet';
@@ -6,6 +8,23 @@ import Header from '../components/Header';
 import '../index.css';
 
 function Map() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api/getData');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
       <Header />
@@ -14,11 +33,15 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[33.6226, -117.9300]} iconSize={[120, 95]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {data.map((marker, index) => (
+          <Marker
+            key={index}
+            position={[marker.spotLat, marker.spotLon]}
+          >
+            <Popup>{marker.spotName}</Popup>
+
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
