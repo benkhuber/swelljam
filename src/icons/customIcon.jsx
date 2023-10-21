18b978d45b3473a738b7ef1437f6eb5fe6b6a4d6
Ben Huber <benkhuber@gmail.com>
2023-10-20 22:01:25 -0700

    added buoy markers to leaflet map, custom icon

---
+++ src/icons/customIcon.jsx
@@ -0,0 +1,10 @@
+import L from 'leaflet';
+
+const customIcon = new L.Icon({
+  iconUrl: '/src/icons/mapMarker.png', // Provide the path to your custom icon image
+  iconSize: [16, 16], // Adjust the size of the icon
+  iconAnchor: [16, 32], // The point where the icon should be anchored
+  popupAnchor: [0, -32], // The point from which the popup should open relative to the icon
+});
+
+export default customIcon;
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -6,20 +6,66 @@ import {
 } from 'react-leaflet';
 import Header from '../components/Header';
 import '../index.css';
+import customIcon from '../icons/customIcon';
 
 function Map() {
   const [data, setData] = useState([]);
+  const [buoyStationData, setBuoyStationData] = useState([]);
+
+  const parseAllStationRawData = (rawBuoyData) => {
+    const xmlString = rawBuoyData;
+    const parsedBuoyStations = [];
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+    const stations = xmlDoc.getElementsByTagName('station');
+
+    for (let i = 0; i < stations.length; i++) {
+      const station = stations[i];
+      const id = station.getAttribute('id');
+      const lat = station.getAttribute('lat');
+      const lon = station.getAttribute('lon');
+      // const elev = station.getAttribute('elev');
+      const name = station.getAttribute('name');
+      // const owner = station.getAttribute('owner');
+      // const pgm = station.getAttribute('pgm');
+      const type = station.getAttribute('type');
+      // const met = station.getAttribute('met');
+      // const currents = station.getAttribute('currents');
+      // const waterquality = station.getAttribute('waterquality');
+      // const dart = station.getAttribute('dart');
+
+      // Checks if station type is buoy. If true adds station info to parsedBuoyStations array
+      if (type === 'buoy' && name.includes(', CA')) {
+        const parsedBuoyStation = {
+          id,
+          lat,
+          lon,
+          name,
+        };
+        parsedBuoyStations.push(parsedBuoyStation);
+      }
+    }
+    setBuoyStationData(parsedBuoyStations);
+    console.log(buoyStationData);
+  };
+
+  const stationApiURL = 'http://localhost:3001/api/buoydata/allstations';
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:3001/api/getData');
+        const buoyStationsResponse = await (fetch(stationApiURL));
+        if (!buoyStationsResponse.ok) {
+          throw new Error('Network response was not ok');
+        }
         setData(response.data);
+        const allBuoyStations = await buoyStationsResponse.text();
+        parseAllStationRawData(allBuoyStations);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-
     fetchData();
   }, []);
 
@@ -40,6 +86,22 @@ function Map() {
                   position={[marker.spotLat, marker.spotLon]}
                 >
                   <Popup>{marker.spotName}</Popup>
+                </Marker>
+              ))}
+            </LayerGroup>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name="Buoys">
+            <LayerGroup>
+              {buoyStationData.map((buoy, index) => (
+                <Marker
+                  key={index}
+                  position={[buoy.lat, buoy.lon]}
+                  icon={customIcon}
+                >
+                  <Popup className="popup">
+                    Station ID: {buoy.id} <br />
+                    Buoy Name: {buoy.name}
+                  </Popup>
                 </Marker>
               ))}
             </LayerGroup>
