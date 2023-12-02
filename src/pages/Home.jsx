9cba2904ba532aa44919527fff77e6abebb54a89
Ben Huber <benkhuber@gmail.com>
2023-12-01 20:54:45 -0800

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import BuoyData from '../BuoyData';
 
 function Home() {
   return (
     <div>
       <Header />
       Home
+      <BuoyData currentStationId="46253" />
     </div>
   );
 }
