6c568cc184acf55666b54f297f4d48aff134a2b6
Ben Huber <benkhuber@gmail.com>
2023-12-14 22:16:45 -0800

    refactored the render card function into separate sessioncard component. React handles delete vs manipulating DOM

---
+++ src/components/SessionCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function SessionCard({ session, onDelete }) {
+  const parseDateTimeToPST = (dateTimeInput) => {
+    const dateTimeUTC = new Date(dateTimeInput);
+    const dateTimePST = dateTimeUTC.toLocaleString(
+      'en-US',
+      {
+        timezone: 'America/Los_Angeles',
+        month: 'numeric',
+        day: 'numeric',
+        year: 'numeric',
+        weekday: 'long',
+        hour: 'numeric',
+        minute: 'numeric',
+      },
+    );
+    return dateTimePST;
+  };
+
+  const dateTimePST = parseDateTimeToPST(session.dateTimeSelect);
+
+  return (
+    <div className="sessionCard" id={session._id}>
+      <div>{dateTimePST}</div>
+      <div>Spot: {session.selectedSpot}</div>
+      <div>Wave Rating: {session.waveRating}</div>
+      <div>Size Rating: {session.sizeRating}</div>
+      <div>Wind Rating: {session.windRating}</div>
+      <div>Crowd Rating: {session.CrowdRating}</div>
+      <div>{session.primarySwellHeight} m at {session.primarySwellPeriod}
+        seconds from {session.primarySwellDirection}
+      </div>
+      <button type="button" onClick={() => onDelete(session._id)}>Delete Session</button>
+    </div>
+  );
+}
+
+export default SessionCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from '../components/Header';
+import SessionCard from '../components/SessionCard';
 
 function Home() {
   const [sessionData, setSessionData] = useState();
@@ -20,11 +21,9 @@ function Home() {
     fetchData();
   }, []);
 
-  const deleteSession = async (e) => {
-    const idToDelete = e.target.parentElement.id;
-    console.log(idToDelete);
+  const deleteSession = async (sessionId) => {
     try {
-      const response = await axios.delete(`http://localhost:3001/api/deleteSession/${idToDelete}`);
+      const response = await axios.delete(`http://localhost:3001/api/deleteSession/${sessionId}`);
       console.log(response.data);
       fetchData();
     } catch (error) {
@@ -32,111 +31,22 @@ function Home() {
     }
   };
 
-  const parseDateTimeToPST = (dateTimeInput) => {
-    const dateTimeUTC = new Date(dateTimeInput);
-    const dateTimePST = dateTimeUTC.toLocaleString(
-      'en-US',
-      {
-        timezone: 'America/Los_Angeles',
-        month: 'numeric',
-        day: 'numeric',
-        year: 'numeric',
-        weekday: 'long',
-        hour: 'numeric',
-        minute: 'numeric',
-      },
-    );
-
-    return dateTimePST;
-  };
-
-  const sessionContainer = document.getElementById('sessionsContainer');
-
-  const addSessionListeners = () => {
-    const cards = document.getElementsByClassName('sessionCard');
-
-    for (const card of cards) {
-      card.addEventListener('click', (e) => {
-        console.log(e.currentTarget.id);
-      });
-    }
-  };
-
-  const renderSessionCards = (sessionDataInput) => {
-    const sessionCard = document.createElement('div');
-    const spotNameCard = document.createElement('div');
-    const dateTimeCard = document.createElement('div');
-    const waveRatingCard = document.createElement('div');
-    const sizeRatingCard = document.createElement('div');
-    const windRatingCard = document.createElement('div');
-    const crowdRatingCard = document.createElement('div');
-    const swellHeightCard = document.createElement('div');
-    const deleteSessionButton = document.createElement('button');
-
-    const dateTimePST = parseDateTimeToPST(sessionDataInput.dateTimeSelect);
-
-    sessionCard.className = 'sessionCard';
-    // eslint-disable-next-line no-underscore-dangle
-    sessionCard.id = sessionDataInput._id;
-
-    dateTimeCard.value = dateTimePST;
-    dateTimeCard.textContent = dateTimePST;
-
-    spotNameCard.value = sessionDataInput.selectedSpot;
-    spotNameCard.textContent = `Spot: ${sessionDataInput.selectedSpot}`;
-
-    waveRatingCard.value = sessionDataInput.waveRating;
-    waveRatingCard.textContent = `Wave Rating: ${sessionDataInput.waveRating}`;
-
-    sizeRatingCard.value = sessionDataInput.sizeRating;
-    sizeRatingCard.textContent = `Size Rating: ${sessionDataInput.sizeRating}`;
-
-    windRatingCard.value = sessionDataInput.windRating;
-    windRatingCard.textContent = `Wind Rating: ${sessionDataInput.windRating}`;
-
-    crowdRatingCard.value = sessionDataInput.crowdRating;
-    crowdRatingCard.textContent = `Crowd Rating: ${sessionDataInput.crowdRating}`;
-
-    swellHeightCard.value = sessionDataInput.primarySwellHeight;
-    swellHeightCard.textContent = `${sessionDataInput.primarySwellHeight} m at ${sessionDataInput.primarySwellPeriod} seconds from ${sessionDataInput.primarySwellDirection} degrees.`;
-
-    deleteSessionButton.textContent = 'Delete Session';
-    deleteSessionButton.addEventListener('click', deleteSession);
-
-    sessionCard.appendChild(dateTimeCard);
-    sessionCard.appendChild(spotNameCard);
-    sessionCard.appendChild(waveRatingCard);
-    sessionCard.appendChild(sizeRatingCard);
-    sessionCard.appendChild(windRatingCard);
-    sessionCard.appendChild(crowdRatingCard);
-    sessionCard.appendChild(swellHeightCard);
-    sessionCard.appendChild(deleteSessionButton);
-
-    sessionContainer.appendChild(sessionCard);
-  };
-
   useEffect(() => {
-    if (sessionData && sessionData.length === 0) {
-      sessionContainer.innerHTML = 'No logged sessions.';
-    } else if (sessionData && sessionData.length > 6) {
-      sessionContainer.innerHTML = '';
-      for (let i = (sessionData.length - 6); i < sessionData.length; i++) {
-        renderSessionCards(sessionData[i]);
-      }
-    } else if (sessionData && sessionData.length >= 0 && sessionData.length <= 6) {
-      sessionContainer.innerHTML = '';
-      for (let i = 0; i < sessionData.length; i++) {
-        renderSessionCards(sessionData[i]);
-      }
-    }
-    addSessionListeners();
   }, [sessionData]);
 
   return (
     <div>
       <Header />
       <h3>Your Recent Sessions</h3>
-      <div id="sessionsContainer" />
+      <div id="sessionsContainer">
+        {sessionData && sessionData.length > 0 ? (
+          sessionData.map((session) => (
+            <SessionCard session={session} onDelete={deleteSession} />
+          ))
+        ) : (
+          <p>No recent sessions.</p>
+        )}
+      </div>
     </div>
   );
 }
